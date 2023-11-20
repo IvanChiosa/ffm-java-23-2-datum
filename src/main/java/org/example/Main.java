58493657a303b26a04784b906f5145d9a2a7d35f
Ivan <i85chiosa@gmail.com>
2023-11-20 11:25:03 +0100

    Date und Times Übung

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,41 @@
 package org.example;
 
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
     public static void main(String[] args) {
+
+        LocalDate localDate = LocalDate.of(2023, 11, 20);
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("EEEE, dd.MMMM.yyyy HH:mm");
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("EEEE, dd.MMMM.yyyy HH:mm:ss");
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofPattern("EEEE, dd.MMMM.yyyy HH:mm:ss Z");
+
+        System.out.println(localDate);
+        System.out.println("Hello" + localTime);
+        System.out.println(localDateTime);
+        System.out.println(zonedDateTime);
+        System.out.println(dateTimeFormatter.format(localDateTime));
+        System.out.println(dateTimeFormatter1.format(localDateTime));
+        System.out.println(dateTimeFormatter2.format(localDateTime));
+        System.out.println(dateTimeFormatter3.format(zonedDateTime));
+
+
+        System.out.println(Instant.now());
+
+        LocalDateTime ldt1 = LocalDateTime.of(2021, 11, 8, 10, 0, 0, 0);
+        LocalDateTime ldt2 = LocalDateTime.of(2023, 11, 8, 10, 0, 0, 0);
+        long days = ChronoUnit.DAYS.between(ldt1, ldt2);
+        long months = ChronoUnit.MONTHS.between(ldt1, ldt2);
+        System.out.println(days);
+        System.out.println(months);
 
     }
 }
---
+++ src/main/java/org/example/repo/Main.java
@@ -0,0 +1,50 @@
+package org.example.repo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Step 1: Write code to output the current date and time.
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String formattedDateTime = localDateTime.format(formatter);
+
+        System.out.println("Current Date und Time: " + formattedDateTime);
+
+
+        // Step 2: Add a timespan of 2 weeks to the current date and output the new date.
+        LocalDateTime newDateTime = localDateTime.plusWeeks(2);
+
+        String formattedNewDateTime = newDateTime.format(formatter);
+
+        System.out.println("New date and Time after adding 2 weeks: " + formattedNewDateTime);
+
+        // Step 3: Compare the current date with a specified future date and
+        // output whether the current date is before or after the specified date.
+        LocalDateTime futureDataTime = LocalDateTime.of(2023, 11, 20, 11, 56);
+
+        String formattedFutureDateTime = futureDataTime.format(formatter);
+        System.out.println("Future Date: " + formattedFutureDateTime);
+
+        if (localDateTime.isBefore(futureDataTime)) {
+            System.out.println("The current date is before the specified future date.");
+        } else if (localDateTime.isAfter(futureDataTime)) {
+            System.out.println("The current date is after the specified future date.");
+        } else {
+            System.out.println("The current date is equal to the specified future date.");
+        }
+
+        // Step 4: Calculate the difference in days between two arbitrary dates and output the result.
+        LocalDateTime date1 = LocalDateTime.of(2023, 11, 20, 11, 30);
+        LocalDateTime date2 = LocalDateTime.of(2023, 12, 20, 11, 30);
+        long daysDifference = ChronoUnit.DAYS.between(date1, date2);
+
+
+        System.out.println("Difference in days between the two dates: " + daysDifference);
+    }
+}
